5ba757bd7ab39c52450d8d2a4cb0e124aea9f94f
Bheddar Zakaria <zakaria.bheddar@student-cs.fr>
2024-12-31 00:50:43 +0100

    sh

--- compile-run.sh
+++ compile-run.sh
@@ -3,17 +3,28 @@
 # Create bin directory if it doesn't exist
 mkdir -p bin
 
+# Function for colored output
+function print_info() {
+    echo -e "\e[32m$1\e[0m"  # Green color for informational messages
+}
+
+function print_error() {
+    echo -e "\e[31m$1\e[0m"  # Red color for error messages
+}
+
 # Compile the Java files
-echo "Compiling Java files..."
-javac -d bin src/model/GameModel.java src/view/GameView.java src/controller/GameController.java src/GuessGame.java
+print_info "================================================================================================================="
+print_info "Compiling Java files..."
+javac -d bin -sourcepath src src/model/GameModel.java src/view/GameView.java src/controller/GameController.java src/GuessGame.java
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
-    echo "Compilation successful!"
-    echo "Running the game..."
+    print_info "Compilation successful!"
+    print_info "Running the game..."
+    print_info "================================================================================================================="
     # Run the program
     java -cp bin GuessGame
 else
-    echo "Compilation failed!"
+    print_error "Compilation failed!"
     exit 1
 fi
